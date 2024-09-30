5ab6badbf17253afcf865cafe9b6823f9c9beed5
PierreOssun <pierre.giraud@mailfence.com>
2024-09-30 19:31:37 +0200

    fix generate Readme

--- generate_readme.js
+++ generate_readme.js
@@ -9,7 +9,7 @@ function getLatestJsonFile() {
 }
 
 function generateReadmeTables(data) {
-    const ethereumNetworks = ['BaseMainnet', 'OPMainnet'];
+    const ethereumNetworks = ['BaseMainnet', 'OpMainnet'];
     const sepoliaNetworks = ['OsakiSepolia', 'MinatoSepolia', 'BaseSepolia', 'OpSepolia'];
 
     function createTable(networks, title) {
